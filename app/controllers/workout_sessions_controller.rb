8d6a4ef5ad75ff6106f4f087d4fa56161b8174a0
Preston Rohner <prestonrohner@me.com>
2011-09-15 21:54:52 -0700

    Add saving a workout session

--- app/controllers/workout_sessions_controller.rb
+++ app/controllers/workout_sessions_controller.rb
@@ -1,5 +1,9 @@
 class WorkoutSessionsController < ApplicationController
   def create
+    @workout_session = WorkoutSession.new(params[:workout_session])
+    respond_to do |format|
+      format.html { redirect_to(users_url, :notice => 'Workout was successfully created.') }
+    end
   end
 
   def delete
--- test/functional/workout_sessions_controller_test.rb
+++ test/functional/workout_sessions_controller_test.rb
@@ -1,9 +1,16 @@
 require 'test_helper'
 
 class WorkoutSessionsControllerTest < ActionController::TestCase
-  test "should get create" do
-    get :create
-    assert_response :success
+  test "should create a new workout session" do
+    #workout_session = workout_sessions(:recent)
+    workout = {
+      :description    => "a workout and all that",
+      :user_id        => 1,
+      :workout_date   => "8/1/2011"
+    }
+
+    post :create, :workout_session => workout
+    assert_redirected_to users_url
   end
 
 end
